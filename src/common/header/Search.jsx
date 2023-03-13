3c6e44af558b0098e88949c1c203e23bd045215e
NVL2001 <1901040121@s.hanu.edu.vn>
2023-03-13 20:47:21 +0700

    homepage v1

--- src/common/header/Search.jsx
+++ src/common/header/Search.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "../../components/assets/images/logo.svg";
+import logo from "../../components/assets/images/logo.png";
 import { Link } from "react-router-dom";
 
 const Search = ({ CartItem }) => {
@@ -14,8 +14,8 @@ const Search = ({ CartItem }) => {
       <section className="search">
         <div className="container c_flex">
           <div className="logo width ">
-            {/* <img src={logo} alt='' /> */}
-            <a href="">BEAUTIFIE</a>
+            <img src={logo} alt="" />
+            {/* <a href="">BEAUTIFIE</a> */}
           </div>
 
           <div className="search-box f_flex">
--- src/components/shops/Shop.jsx
+++ src/components/shops/Shop.jsx
@@ -6,14 +6,14 @@ import "./style.css";
 const Shop = ({ addToCart, shopItems }) => {
   return (
     <>
-      <section className="shop background">
+      <section id="shop" className="shop background">
         <div className="container d_flex">
           {/* <Catg /> */}
 
           <div className="contentWidth">
             <div className="heading d_flex">
               <div className="heading-left row  f_flex">
-                <h2>Mobile Phones</h2>
+                {/* <h2>Mobile Phones</h2> */}
               </div>
               <div className="heading-right row ">
                 <span>View all</span>
